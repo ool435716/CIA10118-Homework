ef16dcd887921982668f9fceaad834701819d8ad
shun <ool435716@gmail.com>
2024-02-20 19:35:39 +0800

    submit hw9

---
+++ src/hw9/Test1.java
@@ -0,0 +1,71 @@
+package hw9;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Test1 {
+	public static void main(String[] args) {
+//		Integer(100)、Double(3.14)、Long(21L)、Short(“100”)、Double(5.1)、“Kitty”、Integer(100)、
+//		Object物件、“Snoopy”、BigInteger(“1000”)
+		List<Object> list = new ArrayList<>();
+		list.add(100);
+		list.add(3.14);
+		list.add(21L);
+		list.add((short)100);
+		list.add(5.1);
+		list.add("Kitty");
+		list.add(100);
+		list.add(new Train(202, "普悠瑪", "樹林", "花蓮", 400));
+		list.add("Snoopy");
+		list.add(new BigInteger("1000"));
+		
+		Iterator<Object> it = list.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+		
+		System.out.println("===========================================");
+		
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+		
+		System.out.println("===========================================");
+		
+		for (Object object : list) {
+			System.out.println(object);
+		}
+		
+		System.out.println("===========================================");
+		
+		
+		List<Number> list1 = new ArrayList<>();
+		list1.add(100);
+		list1.add(3.14);
+		list1.add(21L);
+		list1.add((short)100);
+		list1.add(5.1);
+		list1.add(100);
+		list1.add(new BigInteger("1000"));
+		
+		Iterator<Number> it1 = list1.iterator();
+		while(it1.hasNext()) {
+			System.out.println(it1.next());
+		}
+		
+		System.out.println("===========================================");
+		
+		for (int i = 0; i < list1.size(); i++) {
+			System.out.println(list1.get(i));
+		}
+		
+		System.out.println("===========================================");
+		
+		for (Object object : list1) {
+			System.out.println(object);
+		}
+	}
+	
+}
---
+++ src/hw9/Test4.java
@@ -0,0 +1,36 @@
+package hw9;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Test4 {
+	public static void main(String[] args) {
+		
+		Set<Train> set = new TreeSet<>();
+		set.add(new Train(202, "普悠瑪", "樹林", "花蓮", 400));
+		set.add(new Train(1254, "區間", "屏東", "基隆", 700));
+		set.add(new Train(118, "自強", "高雄", "台北", 500));
+		set.add(new Train(1288, "區間", "新竹", "基隆", 400));
+		set.add(new Train(122, "自強", "台中", "花蓮", 600));
+		set.add(new Train(1222, "區間", "樹林", "七堵", 300));
+		set.add(new Train(1254, "區間", "屏東", "基隆", 700));
+		
+		for (Object object : set) {
+			System.out.println(object);
+		}
+		
+		System.out.println("========================================================================");
+		
+		Iterator<Train> iterator = set.iterator();
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+		
+		System.out.println("========================================================================");
+		
+		for (Iterator<Train> iterator2 = set.iterator(); iterator2.hasNext();) {
+			System.out.println(iterator2.next());
+		}
+	}
+}
